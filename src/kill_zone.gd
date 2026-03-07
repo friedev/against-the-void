f7efc87d2cd9051630e3918210d15604d46ebe8b
Aaron Friesen <aaron@frie.dev>
2026-03-07 12:28:55 -0600

    Despawn faraway enemies

--- src/kill_zone.gd
+++ src/kill_zone.gd
@@ -5,3 +5,10 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		var player: Player = body
 		player.die()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is Enemy:
+		var enemy: Enemy = area
+		# Don't use die() because it triggers screen shake and stuff
+		enemy.queue_free()

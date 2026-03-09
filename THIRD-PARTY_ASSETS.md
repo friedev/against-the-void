7c3ddb879b9dce86a9680c77d107e691cd270f13
Aaron Friesen <aaron@frie.dev>
2026-03-08 20:19:36 -0500

    Add air dash

--- THIRD-PARTY_ASSETS.md
+++ THIRD-PARTY_ASSETS.md
@@ -5,6 +5,7 @@
 - [8-Bit Hit](https://freesound.org/people/TheDweebMan/sounds/277213/) by [TheDweebMan](https://freesound.org/people/TheDweebMan/) ([CC0](http://creativecommons.org/publicdomain/zero/1.0/))
 - [RPG Sound Pack](https://opengameart.org/content/rpg-sound-pack) by [artisticdude](https://opengameart.org/users/artisticdude) ([CC0](http://creativecommons.org/publicdomain/zero/1.0/))
 - [8-bit platformer SFX](https://opengameart.org/content/8-bit-platformer-sfx) by [bart](https://opengameart.org/users/bart) ([CC-BY 3.0](http://creativecommons.org/licenses/by/3.0/))
+- [8-Bit Sound Effect Pack (Vol. 001)](https://opengameart.org/content/8-bit-sound-effect-pack-vol-001) by [@Shades](https://soundcloud.com/noshades) ([CC0](http://creativecommons.org/publicdomain/zero/1.0/))
 - [Dark Ambience](https://freesound.org/people/PatrickLieberkind/sounds/244961/) by [PatrickLieberkind](https://freesound.org/people/PatrickLieberkind/) ([CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/))
 - [Wind, Synthesized, A.wav](https://freesound.org/people/InspectorJ/sounds/376415/) by [InspectorJ](http://www.jshaw.co.uk/) ([CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/))
 
--- src/player.gd
+++ src/player.gd
@@ -7,12 +7,14 @@ static var instance: Player
 @export var jump_speed: float
 @export var pogo_recoil_speed: float
 @export var non_pogo_recoil_speed: float
+@export var dash_speed: float
 @export var acceleration: float
 @export var deceleration: float
 @export var fast_fall_acceleration: float
 @export var attack_duration: float
 @export var attack_cooldown: float
 @export var air_jumps: int
+@export var air_dashes: int
 
 @export_group("Internal Nodes")
 @export var sprite: AnimatedSprite2D
@@ -20,15 +22,19 @@ static var instance: Player
 @export var camera: Camera2D
 @export var attack_sound: SmartSound
 @export var jump_sound: SmartSound
+@export var dash_sound: SmartSound
 @export var death_sound: SmartSound
 @export var jump_particles: GPUParticles2D
+@export var dash_particles: GPUParticles2D
 @export var death_particles: GPUParticles2D
 
 @onready var initial_position := position
 @onready var jumps_left := air_jumps
+@onready var dashes_left := air_dashes
 
 var is_attack_buffered := false
 var is_attacking := false
+var last_input_direction := 1.0
 var animation_state := 0
 
 func _enter_tree() -> void:
@@ -45,34 +51,32 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	velocity.y = minf(velocity.y, max_speed.y)
 
-	# Handle jump
-	if Input.is_action_just_pressed("jump"):
-		jump()
-	elif not Input.is_action_pressed("jump") and velocity.y < 0:
-		# Variable jump height: quickly stop going up if jump is released
+	# Variable jump height: quickly stop going up if jump is released
+	if not Input.is_action_pressed("jump") and velocity.y < 0:
 		velocity.y += fast_fall_acceleration * delta
 
 	var input_direction := Input.get_axis("move_left", "move_right")
 	var target_speed := input_direction * max_speed.x
 	var current_acceleration := deceleration
-	if input_direction != 0.0 and (
-		sign(target_speed) == sign(velocity.x) and abs(target_speed) > abs(velocity.x)
-	):
-		current_acceleration = acceleration
+	if input_direction != 0.0:
+		last_input_direction = input_direction
+		if signf(target_speed) == signf(velocity.x) and absf(target_speed) > absf(velocity.x):
+			current_acceleration = acceleration
 	velocity.x = move_toward(velocity.x, target_speed, current_acceleration * delta)
 
 	move_and_slide()
 
-	if not is_attacking:
-		if input_direction < 0.0:
-			sprite.flip_h = true
-		elif input_direction > 0.0:
-			sprite.flip_h = false
+	if not is_attacking and input_direction != 0.0:
+		sprite.flip_h = input_direction < 0.0
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("attack"):
+	if event.is_action_pressed("jump"):
+		jump()
+	elif event.is_action_pressed("attack"):
 		attack()
+	elif event.is_action_pressed("dash"):
+		dash()
 	elif event.is_action_pressed("die"):
 		die()
 
@@ -98,7 +102,7 @@ func attack() -> void:
 	if attack_direction.y != 0.0:
 		attack_direction.x = 0.0
 	if attack_direction == Vector2.ZERO:
-		attack_direction = Vector2.LEFT if sprite.flip_h else Vector2.RIGHT
+		attack_direction = Vector2(last_input_direction, 0)
 	attack_direction = attack_direction.normalized()
 
 	sword_area.rotation = attack_direction.angle()
@@ -117,6 +121,17 @@ func attack() -> void:
 	if is_attack_buffered:
 		is_attack_buffered = false
 		attack()
+
+
+func dash() -> void:
+	if dashes_left == 0:
+		return
+	dashes_left -= 1
+	velocity.x += last_input_direction * dash_speed
+	velocity.y = 0
+	dash_sound.randomize_and_play()
+	dash_particles.scale.x = last_input_direction
+	dash_particles.restart()
 
 
 func die() -> void:
@@ -148,6 +163,7 @@ func hit(enemy: Enemy) -> void:
 	if recoil_direction.y > 0:
 		velocity.y = - pogo_recoil_speed
 		jumps_left = air_jumps
+		dashes_left = air_dashes
 	else:
 		velocity -= recoil_direction * non_pogo_recoil_speed
 

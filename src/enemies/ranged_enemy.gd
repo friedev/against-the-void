f900762b90e4e36a2b82393ac613e96c1ebc2f8c
Aaron Friesen <aaron@frie.dev>
2026-03-14 00:05:35 -0500

    Balance projectile enemy
    - Separate horizontal and vertical ranges
    - Increase projectile speed
    - Update projectile sprite
    - Slightly adjust hitbox

--- src/enemies/ranged_enemy.gd
+++ src/enemies/ranged_enemy.gd
@@ -1,8 +1,8 @@
 class_name RangedEnemy extends Enemy
 
 @export var speed: float
-@export var min_distance: float
-@export var max_distance: float
+@export var min_distance: Vector2
+@export var max_distance: Vector2
 @export var osc_period: float
 @export var osc_amplitude: float
 @export var projectile_scene: PackedScene
@@ -23,21 +23,22 @@ func _physics_process(delta: float) -> void:
 	var prev_osc_input := osc_input
 	osc_input += delta / osc_period
 	position.y += (sin(osc_input * TAU) - sin(prev_osc_input * TAU)) * osc_amplitude
-	if Player.instance != null:
+	if Player.instance != null and not is_in_range(Player.instance.global_position, min_distance):
 		var angle_to_player := global_position.angle_to_point(Player.instance.global_position)
-		var target_distance := global_position.distance_to(Player.instance.global_position) - min_distance
-		var movement := Vector2(speed * delta, 0).rotated(angle_to_player).limit_length(maxf(target_distance, 0.0))
+		var movement := Vector2(speed * delta, 0).rotated(angle_to_player)
 		position += movement
 		if movement.x != 0:
 			scale.x = signf(movement.x)
 
 
 func _on_shoot_timer_timeout() -> void:
-	if Player.instance != null:
-		var distance_squared := global_position.distance_squared_to(Player.instance.global_position)
-		if distance_squared <= max_distance ** 2:
-			shoot()
+	if Player.instance != null and is_in_range(Player.instance.global_position, max_distance):
+		shoot()
 
+
+func is_in_range(target_position: Vector2, target_range: Vector2) -> bool:
+	var to_target := (global_position - target_position).abs()
+	return to_target.x <= target_range.x and to_target.y <= target_range.y
 
 func shoot() -> void:
 	var projectile: Node2D = projectile_scene.instantiate()

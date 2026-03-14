560180800b1f5d5b275a9f983e26062544437a7a
Aaron Friesen <aaron@frie.dev>
2026-03-14 10:59:02 -0500

    Add acceleration and inertia to seeker enemy

--- src/enemies/seeker_enemy.gd
+++ src/enemies/seeker_enemy.gd
@@ -1,16 +1,14 @@
 class_name SeekerEnemy extends Enemy
 
-@export var min_speed: float
 @export var max_speed: float
 @export var acceleration: float
 
 var velocity: Vector2
 
-@onready var speed := randf_range(min_speed, max_speed)
-
 func _physics_process(delta: float) -> void:
 	if Player.instance != null:
-		velocity = Vector2(speed * delta, 0).rotated(global_position.angle_to_point(Player.instance.global_position))
-	position += velocity
+		var velocity_change := Vector2(acceleration * delta, 0).rotated(global_position.angle_to_point(Player.instance.global_position))
+		velocity = (velocity + velocity_change).limit_length(max_speed)
+	position += velocity * delta
 	if velocity.x != 0:
 		scale.x = signf(velocity.x)
